3987fdf63af13a37687cbd4de8f564f065acf984
Icaroiz <icaroizext@gmail.com>
2023-02-15 12:19:12 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,16 +9,19 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/View.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
 			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-
-			Scene mainScene = new Scene(scrollPane);
+			
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -27,6 +30,10 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
